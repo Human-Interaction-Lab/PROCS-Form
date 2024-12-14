e06be96cadb57d18efb0a02f7c154a73d36485b8
Tyson Barrett <t.barrett88@gmail.com>
2024-12-14 11:37:00 -0700

    update to new site

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://tysonbarrett.com/PROCS-Form/',
+  base: 'https://human-interaction-lab.github.io/PROCS-Form/',
   build: {
     outDir: 'docs'
   },
